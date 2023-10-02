6d08ae72d6e2109e88e96e9f1d304397a754d015
sulistyp <sulisdeveloper@gmail.,com>
2023-10-02 17:14:45 +0700

    query builder Grouping with having

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -356,5 +356,60 @@ class QueryBuilderTest extends TestCase
         self::assertEquals(20000000, $collection[0]->max_price);
     }
 
+    public function insertProductFood()
+    {
+        DB::table('products')->insert([
+            "id" => "3",
+            "name" => "Bakso",
+            "category_id" => "FOOD",
+            "price" => 20000
+        ]);
+
+        DB::table('products')->insert([
+            "id" => "4",
+            "name" => "Mie Ayam",
+            "category_id" => "FOOD",
+            "price" => 20000
+        ]);
+    }
+
+    public function testGroupBy()
+    {
+        $this->insertProducts();
+        $this->insertProductFood();
+
+        $collection = DB::table("products")
+            ->select("category_id", DB::raw("count(*) as total_product"))
+            ->groupBy("category_id")
+            ->orderBy("category_id", "desc")
+            ->get();
+
+        self::assertCount(2, $collection);
+        self::assertEquals("SMARTPHONE", $collection[0]->category_id);
+        self::assertEquals("FOOD", $collection[1]->category_id);
+        self::assertEquals(2, $collection[0]->total_product);
+        self::assertEquals(2, $collection[0]->total_product);
+
+        $collection->each(function ($item){
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function testGroupByHaving()
+    {
+        $this->insertProducts();
+        $this->insertProductFood();
+
+        $collection = DB::table("products")
+            ->select("category_id", DB::raw("count(*) as total_product"))
+            ->groupBy("category_id")
+            ->orderBy("category_id", "desc")
+            ->having(DB::raw("count(*)"), ">", 2)
+            ->get();
+
+        self::assertCount(0, $collection);
+
+    }
+
 
 }
