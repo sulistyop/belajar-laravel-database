b76765905931e7d231e9188d4cae56d650d6b802
sulistyp <sulisdeveloper@gmail.,com>
2023-10-02 16:25:18 +0700

    query builder JOIN

--- database.sql
+++ database.sql
@@ -21,3 +21,19 @@ create table counters
 insert into counters(id, counter) values ('sample', 0);
 
 select * from counters;
+
+create table products
+(
+    id varchar(100) not null primary key ,
+    name varchar(100) not null ,
+    description text null ,
+    price int not null ,
+    category_id varchar(100) not null ,
+    created_at timestamp not null default current_timestamp,
+    constraint fk_category_id foreign key (category_id) references categories(id)
+) engine innodb;
+
+
+select * from products;
+
+select products.id, products.name, products.price, c.name as category_name from products join categories c on products.category_id = c.id
